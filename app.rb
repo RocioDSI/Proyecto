0da1567c74c054c87bb5249c79d911d72064d860
RocioDSI <alu0100592832@ull.etsii.ull.es>
2014-12-02 14:11:05 +0000

    Migración de la base de datos

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'sinatra/redirect_with_flash'
 enable :sessions
 
 class Post < ActiveRecord::Base
-  validates :title, presence: true, length: {minimun: 5}
+  validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
 end
 
